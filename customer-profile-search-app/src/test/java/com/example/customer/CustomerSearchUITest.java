02a349abb3bdc50636484be5317408d401418ca8
Sujeet <86961377+sujeetsrivastava1997@users.noreply.github.com>
2026-02-19 16:28:42 +0530

    Update CustomerSearchUITest.java

--- customer-profile-search-app/src/test/java/com/example/customer/CustomerSearchUITest.java
+++ customer-profile-search-app/src/test/java/com/example/customer/CustomerSearchUITest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
